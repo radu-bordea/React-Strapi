1444b4bbb0e1d6938dd556a77fecc2176b9705fd
radu-bordea <radu.bordea.dev@gmail.com>
2023-12-03 13:49:40 +0100

    finish submenu

--- src/Context.jsx
+++ src/Context.jsx
@@ -4,6 +4,7 @@ const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [pageId, setPageId] = useState(null);
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -14,7 +15,7 @@ export const AppProvider = ({ children }) => {
   };
 
   return (
-    <AppContext.Provider value={{ isSidebarOpen, openSidebar, closeSidebar }}>
+    <AppContext.Provider value={{ isSidebarOpen, openSidebar, closeSidebar, pageId, setPageId }}>
       {children}
     </AppContext.Provider>
   );
